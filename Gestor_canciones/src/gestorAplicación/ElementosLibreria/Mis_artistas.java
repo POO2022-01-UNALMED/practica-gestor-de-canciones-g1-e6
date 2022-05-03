4719106413f4c2c8071e80a25018860c6d25a001
David Toro Arango <92808109+DavidToro1000@users.noreply.github.com>
2022-05-03 16:59:04 -0500

    Update Mis_artistas.java

--- Gestor_canciones/src/gestorAplicación/ElementosLibreria/Mis_artistas.java
+++ Gestor_canciones/src/gestorAplicación/ElementosLibreria/Mis_artistas.java
@@ -1,5 +1,41 @@
 package ElementosLibreria;
-
+import java.util.ArrayList;
+import Personas.*;
 public class Mis_artistas {
-
+    private ArrayList<Artista> artistas;
+    private Usuario usuario;
+    public Mis_artistas(Usuario usuario) {
+    	this.usuario=usuario;
+    }
+	public ArrayList<Artista> getArtistas() {
+		return artistas;
+	}
+	public void setArtistas(ArrayList<Artista> artistas) {
+		this.artistas = artistas;
+	}
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+    public void agg_artista(Artista artista) {
+    	artistas.add(artista);
+    }
+    public String elim_artista(Artista artista) {
+    	boolean presente=false;
+    	for (Artista c: artistas) {
+    		if (artista==c){
+    			presente=true; 
+    		}
+    
+    	}
+    	if (presente==true) {
+    		artistas.remove(artista);
+    		return"artista eliminado de mis artistas";
+    	}
+    	else {
+    		return"ERROR: el artista no está en mis artistas";
+    	}
+    }
 }
