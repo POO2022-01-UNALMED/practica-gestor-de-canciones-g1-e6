837973becad321d6dc0ece93eff2d209268cd05f
ManuelGallegoJ <92808245+ManuelGallegoJ@users.noreply.github.com>
2022-05-09 19:35:49 -0500

    Para pelar pollos

--- Gestor_canciones/src/gestorAplicación/ElementosLibreria/Genero.java
+++ Gestor_canciones/src/gestorAplicación/ElementosLibreria/Genero.java
@@ -1,5 +1,5 @@
 package ElementosLibreria;
 
 public enum Genero {
-    ROCK(), REGGAETON(), SALSA(), POPULAR(), MERENGUE(), RAP();
+    ROCK(), REGGAETON(), SALSA(), POPULAR(), MERENGUE(), RAP(), MUSICA_PARA_PELAR_POLLOS();
 }
