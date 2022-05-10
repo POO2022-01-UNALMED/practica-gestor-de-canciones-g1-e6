662aab6f14b4739867d3612bac3c41175c3e7d35
David Toro Arango <92808109+DavidToro1000@users.noreply.github.com>
2022-05-10 17:14:34 -0500

    Update Favoritos.java

--- Gestor_canciones/src/gestorAplicación/ElementosLibreria/Favoritos.java
+++ Gestor_canciones/src/gestorAplicación/ElementosLibreria/Favoritos.java
@@ -7,6 +7,6 @@ import Personas.Usuario;
 public class Favoritos extends Playlist{
 	
 	public Favoritos(Usuario creador){
-		super("Favoritos", new ArrayList<Cancion>(), 0, 0, creador, 2);
+		super(creador, "Favoritos");
 	}
 }
