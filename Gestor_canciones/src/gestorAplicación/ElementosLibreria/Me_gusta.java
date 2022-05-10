fe649d40065d4be131b9f97fec37357913e0afac
ManuelGallegoJ <92808245+ManuelGallegoJ@users.noreply.github.com>
2022-05-09 23:32:50 -0500

    Nota

--- Gestor_canciones/src/gestorAplicación/ElementosLibreria/Me_gusta.java
+++ Gestor_canciones/src/gestorAplicación/ElementosLibreria/Me_gusta.java
@@ -6,6 +6,7 @@ public class Me_gusta extends Playlist{
 	//Cómo hago para que no se puedan crear más objetos de esta clase? servirá el final?
 	public Me_gusta(Usuario creador){
 		super(creador, "Me gusta");
+		//Set del id ya que queremos que sea la uno
 		setId_playlist(1);
 	}
 	//Falta hacer esto
