c7f7634a156c7908781f180b98e439c9cf2df579
ManuelGallegoJ <92808245+ManuelGallegoJ@users.noreply.github.com>
2022-05-11 11:08:11 -0500

    Update Me_gusta.java

--- Gestor_canciones/src/gestorAplicacion/ElementosLibreria/Me_gusta.java
+++ Gestor_canciones/src/gestorAplicacion/ElementosLibreria/Me_gusta.java
@@ -1,11 +1,27 @@
 package ElementosLibreria;
 
+import java.util.ArrayList;
+import java.io.Serializable;
 import Personas.Usuario;
 
 public class Me_gusta extends Playlist{
+	
+	private static final long serialVersionUID = 1L;
+	private static ArrayList<Me_gusta> me_gustas;
+	static {
+		me_gustas = new ArrayList<Me_gusta>();
+	}
+	
+	public static ArrayList<Me_gusta> getMe_gustas() {
+		return me_gustas;
+	}
+	public static void setMe_gustas(ArrayList<Me_gusta> me_gustas) {
+		Me_gusta.me_gustas = me_gustas;
+	}
 	//Cómo hago para que no se puedan crear más objetos de esta clase? servirá el final?
 	public Me_gusta(Usuario creador){
 		super(creador, "Me gusta");
+		me_gustas.add(this);
 		//Set del id ya que queremos que sea la uno
 	}
 	//Falta hacer esto
