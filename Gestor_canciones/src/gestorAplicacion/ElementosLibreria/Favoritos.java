d38f9e5cf0fb146c7795229a753344bb30b337f8
ManuelGallegoJ <92808245+ManuelGallegoJ@users.noreply.github.com>
2022-05-11 11:08:04 -0500

    Update Favoritos.java

--- Gestor_canciones/src/gestorAplicacion/ElementosLibreria/Favoritos.java
+++ Gestor_canciones/src/gestorAplicacion/ElementosLibreria/Favoritos.java
@@ -1,12 +1,28 @@
 package ElementosLibreria;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
 import Personas.Usuario;
 
 public class Favoritos extends Playlist{
 	
+	private static final long serialVersionUID = 1L;
+	private static ArrayList<Favoritos> favoritos;
+	static {
+		favoritos = new ArrayList<Favoritos>();
+	}
+	
+	public static ArrayList<Favoritos> getFavoritos() {
+		return favoritos;
+	}
+
+	public static void setFavoritos(ArrayList<Favoritos> favoritos) {
+		Favoritos.favoritos = favoritos;
+	}
+
 	public Favoritos(Usuario creador){
 		super(creador, "Favoritos");
+		favoritos.add(this);
 	}
 }
