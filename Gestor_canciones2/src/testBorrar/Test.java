0793d2cdb22442a6059ac650278e1a37ec3384c1
ManuelGallegoJ <92808245+ManuelGallegoJ@users.noreply.github.com>
2022-05-13 09:56:37 -0500

    Serializado

--- Gestor_canciones2/src/testBorrar/Test.java
+++ Gestor_canciones2/src/testBorrar/Test.java
@@ -6,7 +6,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		Deserializador.deserializarTodo();
-		
+		Artista test3 = new Artista("El pela pollos de Antioquia", null);
 		for(Artista i: Artista.getArtistas()) {
 			System.out.println(i.getNombre());
 		}
