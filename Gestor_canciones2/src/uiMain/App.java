a259f2d158ecaa81ccf6129ce5cb8776c427f7d3
David Toro Arango <92808109+DavidToro1000@users.noreply.github.com>
2022-05-25 15:41:22 -0500

    Update App.java

--- Gestor_canciones2/src/uiMain/App.java
+++ Gestor_canciones2/src/uiMain/App.java
@@ -26,26 +26,16 @@ public class App {
 	}
 	public static void main(String[] args) {
 		Deserializador.deserializarTodo();
-		System.out.println("Buenos dias Administrador\n");
-		System.out.println("los usuarios creados son los siguientes:");
-		for (Usuario usuario : Usuario.getUsuarios()) {
-			System.out.println(usuario.datosPersona());
-		}
-		for (Artista artista : Artista.getArtistas()) {
-			System.out.println(artista.datosPersona());
-		}
+		System.out.println("Saludos!,\n");
 		int opcion;
 		do {
-			System.out.println("Que desea hacer?");
+			System.out.println("Que accion desea realizar?");
 			System.out.println(" 1. Crear usuario");
-			System.out.println(" 2. Agregar cancion a playlist");
-			System.out.println(" 3. Recomendar cancion a un usuario");
-			System.out.println(" 4. Reproducir canción aleatoria de una playlist");
-			System.out.println(" 5. Mostrar duración total de todas las canciones en una playlist.");
-			System.out.println(" 6. Indicar si una canción se repite en varias playlists.");
-			System.out.println(" 7. Generar una playlist con canciones en me gusta de un artista en especifico.");
-			System.out.println(" 8. Mostrar canciones existentes");
-			System.out.println(" 9. Guardar y cerrar");
+			System.out.println(" 2. Recomendar cancion a un usuario");
+			System.out.println(" 3. Desplegar Biblioteca de un usuario");
+			System.out.println(" 4. Mostrar canciones existentes");
+			System.out.println(" 5. Mostrar usuarios/artistas existentes");
+			System.out.println(" 6. Guardar y cerrar");
 			
 			System.out.println("Elija una opcion: ");
 			opcion = (int) readInt();
@@ -56,32 +46,35 @@ public class App {
 				//menuCreacionUsuario();
 				break;
 			case 2:
-				//menuAgregarCancion();
-				break;
-			case 3:
 				//menuRecomendarCancion();
 				break;
+			case 3:
+				//PedirNombre();
+				//LA IDEA ES QUE SOLICITE NOMBRE DE USUARIO Y LUEGO PUEDA DESPLEGAR TODAS LAS PLAYLIST QUE TIENE AÑADIDAS, SE DEBERA INCLUIR OPCION DE AGG O ELIM CANCION DE UNA PLAYLIST Y TAMBIEN REPRODUCIR POR NOMBRE O ALEATORIO.
+				//ESTE MENU TAMBIEN DEBE TENER LAS OPCIONES:
+				//1. Mostrar duración total de todas las canciones en una playlist.
+				//2. Indicar si una canción se repite en varias playlists.
+				//3. Generar una playlist con canciones en me gusta de un artista en especifico.
+				break;
 			case 4:
-				//menuReproAleatoria();
-				break;
-			case 5:
-				//menuDuracionPlaylist();
-				break;
-			case 6:
-				//menuRepeticionCanciones();
-			case 7:
-				//MenuGeneracionPlaylist();
-				break;
-			case 8:
 				for (Cancion cancion : Cancion.getCancionesCreadas()) {
 					System.out.println(cancion.getNombre());
 				}
 				break;
-			case 9:
+			case 5:
+				System.out.println("los usuarios creados son los siguientes:");
+				for (Usuario usuario : Usuario.getUsuarios()) {
+					System.out.println(usuario.datosPersona());
+				}
+				for (Artista artista : Artista.getArtistas()) {
+					System.out.println(artista.datosPersona());
+				}
+				break;
+			case 6:
 				guardar();
 				System.out.println("Vuelve pronto");
 			}
-			if (opcion != 9) {
+			if (opcion != 6) {
 				System.out.println("\nPresione Enter para continuar");
 				try {
 					System.in.read();
@@ -89,7 +82,7 @@ public class App {
 					e.printStackTrace();
 				}
 			}
-		} while (opcion != 9);
+		} while (opcion != 6);
 
 	}
     public static void guardar() {
