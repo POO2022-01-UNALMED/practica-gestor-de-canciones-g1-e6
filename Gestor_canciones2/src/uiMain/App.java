6b2fe7985705a796ff5a38723d1d3f6d2a561bc7
David Toro Arango <92808109+DavidToro1000@users.noreply.github.com>
2022-05-28 17:24:22 -0500

    Si con esto no compila me muero

--- Gestor_canciones2/src/uiMain/App.java
+++ Gestor_canciones2/src/uiMain/App.java
@@ -26,6 +26,7 @@ public class App {
 	}
 	public static void main(String[] args) {
 		Deserializador.deserializarTodo();
+		inicializar();
 		System.out.println("\n                               ===¡Saludos!===\n");
 		System.out.println("  ********  *******    *******   ********** **  ********   *******     **    **");
 		System.out.println("  **//////  /**////**  **/////** /////**/// /** /**/////   **/////**  //**  **");
@@ -107,7 +108,26 @@ public class App {
 		} while (opcion != 6);
 
 	}
-	
+	static void inicializar() {
+		Cancion.setCancionesCreadas((new ArrayList<Cancion>()));
+		Usuario.setUsuarios((new ArrayList<Usuario>()));
+		Artista.setArtistas((new ArrayList<Artista>()));
+		Usuario test9 = new Usuario("Carlos");
+		Usuario test= new Usuario("Manuel");
+		Usuario test1= new Usuario("PachoGOD");
+		Artista test6=new Artista("El rompe traperas", "Baby brrr brrrr", Genero.REGGAETON);
+		Artista test7=new Artista("bad chicken", "tssss", Genero.MUSICA_PARA_PELAR_POLLOS);
+		Artista test8=new Artista("el motila abuelas", "yeah", Genero.MUSICA_PARA_TUSAR_CALVOS);
+		Cancion test2=new Cancion("A lavar el patio", 180, test6, Genero.REGGAETON);
+		Cancion test3=new Cancion("El patito Juan", 180, test7, Genero.MUSICA_PARA_PELAR_POLLOS);
+		Cancion test4= new Cancion("Vamo a trapear", 120, test8, Genero.MUSICA_PARA_PELAR_POLLOS);
+		Cancion test5= new Cancion("A pelar pollos", 120, test6, Genero.MUSICA_PARA_PELAR_POLLOS);
+		Cancion test10=new Cancion("guelkon turi yangol", 120, test8, Genero.ROCK);
+		test9.agg_Megusta(test2);
+		test9.agg_Megusta(test3);
+		test9.agg_Megusta(test4);
+		test9.agg_Megusta(test10);
+	}
 	static void creacionUsuario() {
 		System.out.println("Ingrese nombre del nuevo usuario:");
 		readString();
