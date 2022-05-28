df36606444b5e4d48f746c7184e8498d272ea85e
ManuelGallegoJ <92808245+ManuelGallegoJ@users.noreply.github.com>
2022-05-28 17:07:00 -0500

    .

--- Gestor_canciones2/src/uiMain/App.java
+++ Gestor_canciones2/src/uiMain/App.java
@@ -26,7 +26,7 @@ public class App {
 	}
 	public static void main(String[] args) {
 		Deserializador.deserializarTodo();
-		System.out.println("\n                               ===Â¡Saludos!===\n");
+		System.out.println("\n                               ===¡Saludos!===\n");
 		System.out.println("  ********  *******    *******   ********** **  ********   *******     **    **");
 		System.out.println("  **//////  /**////**  **/////** /////**/// /** /**/////   **/////**  //**  **");
 		System.out.println(" /**        /**   /** **     //**    /**    /** /**       **     //**  //****");
