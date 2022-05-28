348ebebe29ded5fe04878dfe16c736468a163479
ManuelGallegoJ <92808245+ManuelGallegoJ@users.noreply.github.com>
2022-05-27 22:23:38 -0500

    Presentación

--- Gestor_canciones2/src/uiMain/App.java
+++ Gestor_canciones2/src/uiMain/App.java
@@ -26,7 +26,16 @@ public class App {
 	}
 	public static void main(String[] args) {
 		Deserializador.deserializarTodo();
-		System.out.println("Saludos!,\n");
+		System.out.println("\n                               ===¡Saludos!===\n");
+		System.out.println("  ********  *******    *******   ********** **  ********   *******     **    **");
+		System.out.println("  **//////  /**////**  **/////** /////**/// /** /**/////   **/////**  //**  **");
+		System.out.println(" /**        /**   /** **     //**    /**    /** /**       **     //**  //****");
+		System.out.println(" /********* /******* /**      /**    /**    /** /******* /**      /**   //** ");
+		System.out.println(" ////////** /**////  /**      /**    /**    /** /**////  /**      /**    /** ");
+		System.out.println("        /** /**      //**     **     /**    /** /**      //**     **     /**  ");
+		System.out.println("  ********  /**       //*******      /**    /** /**       //*******      /** ");
+		System.out.println(" ////////   //         ///////       //     //  //         ///////       // \n");
+		                                                         	                                    
 		int opcion;
 		do {
 			System.out.println("Que accion desea realizar?");
