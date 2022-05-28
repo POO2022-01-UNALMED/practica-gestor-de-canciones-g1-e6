bdac5d10702542809cb44ca89261a78276866d40
ManuelGallegoJ <92808245+ManuelGallegoJ@users.noreply.github.com>
2022-05-27 22:47:45 -0500

    Diseño básico de CLI

--- Gestor_canciones2/src/uiMain/App.java
+++ Gestor_canciones2/src/uiMain/App.java
@@ -26,16 +26,25 @@ public class App {
 	}
 	public static void main(String[] args) {
 		Deserializador.deserializarTodo();
-		System.out.println("Saludos!,\n");
+		System.out.println("\n                               ===¡Saludos!===\n");
+		System.out.println("  ********  *******    *******   ********** **  ********   *******     **    **");
+		System.out.println("  **//////  /**////**  **/////** /////**/// /** /**/////   **/////**  //**  **");
+		System.out.println(" /**        /**   /** **     //**    /**    /** /**       **     //**  //****");
+		System.out.println(" /********* /******* /**      /**    /**    /** /******* /**      /**   //** ");
+		System.out.println(" ////////** /**////  /**      /**    /**    /** /**////  /**      /**    /** ");
+		System.out.println("        /** /**      //**     **     /**    /** /**      //**     **     /**  ");
+		System.out.println("  ********  /**       //*******      /**    /** /**       //*******      /** ");
+		System.out.println(" ////////   //         ///////       //     //  //         ///////       // \n");
+		                                                         	                                    
 		int opcion;
 		do {
-			System.out.println("Que accion desea realizar?");
+			System.out.println("Que accion desea realizar?\n");
 			System.out.println(" 1. Crear usuario");
 			System.out.println(" 2. Recomendar cancion a un usuario");
 			System.out.println(" 3. Desplegar Biblioteca de un usuario");
 			System.out.println(" 4. Mostrar canciones existentes");
 			System.out.println(" 5. Mostrar usuarios/artistas existentes");
-			System.out.println(" 6. Guardar y cerrar");
+			System.out.println(" 6. Guardar y cerrar\n");
 			
 			System.out.println("Elija una opcion: ");
 			opcion = (int) readInt();
@@ -43,12 +52,15 @@ public class App {
 			switch (opcion) {
 
 			case 1:
+				System.out.println("-----------------------------------------");
 				creacionUsuario();
 				break;
 			case 2:
+				System.out.println("-----------------------------------------");
 				recomendacion();
 				break;
 			case 3:
+				System.out.println("-----------------------------------------");
 				//PedirNombre();
 				//LA IDEA ES QUE SOLICITE NOMBRE DE USUARIO Y LUEGO PUEDA DESPLEGAR TODAS LAS PLAYLIST QUE TIENE AÃ‘ADIDAS, SE DEBERA INCLUIR OPCION DE AGG O ELIM CANCION DE UNA PLAYLIST, crear playlist Y TAMBIEN REPRODUCIR POR NOMBRE O ALEATORIO.
 				//ESTE MENU TAMBIEN DEBE TENER LAS OPCIONES:
@@ -57,24 +69,32 @@ public class App {
 				//3. Generar una playlist con canciones en me gusta de un artista en especifico.
 				break;
 			case 4:
+				System.out.println("-----------------------------------------");
 				for (Cancion cancion : Cancion.getCancionesCreadas()) {
 					System.out.println(cancion.getNombre());
 				}
 				break;
 			case 5:
-				System.out.println("los usuarios creados son los siguientes:");
+				System.out.println("-----------------------------------------");
+				System.out.println("los usuarios creados son los siguientes:\n");
 				for (Usuario usuario : Usuario.getUsuarios()) {
 					System.out.println(usuario.datosPersona());
+					System.out.println();
 				}
+				System.out.println("-----------------------------------------");
+				System.out.println("los artistas creados son los siguientes:\n");
 				for (Artista artista : Artista.getArtistas()) {
 					System.out.println(artista.datosPersona());
+					System.out.println();
 				}
+				System.out.println("-----------------------------------------");
 				break;
 			case 6:
 				guardar();
 				System.out.println("Vuelve pronto");
 			}
 			if (opcion != 6) {
+				System.out.println();
 				System.out.println("\nPresione Enter para continuar");
 				try {
 					System.in.read();
@@ -85,6 +105,7 @@ public class App {
 		} while (opcion != 6);
 
 	}
+	
 	static void creacionUsuario() {
 		System.out.println("Ingrese nombre del nuevo usuario:");
 		readString();
@@ -98,6 +119,7 @@ public class App {
 		new Usuario(nombre);
 		System.out.println("Usuario "+nombre+" creado exitosamente");
 	}
+	
 	static void recomendacion() {
 		System.out.println("Ingrese nombre del usuario a recomendar:");
 		readString();
@@ -111,6 +133,7 @@ public class App {
 		}
 		System.out.println("el usuario ingresado no existe");
 	}
+	
     public static void guardar() {
 	    Serializador.serializarTodo();
     }
