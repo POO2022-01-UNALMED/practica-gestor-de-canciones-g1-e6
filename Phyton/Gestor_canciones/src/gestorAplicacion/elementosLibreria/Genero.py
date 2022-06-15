8cb20ea1b02da135eec4b034668d5179bc1fd04b
ManuelGallegoJ <92808245+ManuelGallegoJ@users.noreply.github.com>
2022-06-15 11:10:33 -0500

    Enum

---
+++ Phyton/Gestor_canciones/src/gestorAplicacion/elementosLibreria/Genero.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class Genero(Enum):
+    ROCK = "Rock"
+    REGGAETON = "Reggaton"
+    SALSA = "Salsa"
+    RAP = "Rap"
+    MUSICA_PARA_PELAR_POLLOS = "Musica para pelar pollos"
+    MUSICA_PARA_TUSAR_CALVOS = "Musica para tusar calvos"
--- Phyton/Gestor_canciones/src/gestorAplicacion/elementosLibreria/Me_gusta.py
+++ Phyton/Gestor_canciones/src/gestorAplicacion/elementosLibreria/Me_gusta.py
@@ -1,4 +1,7 @@
 from Playlist import Playlist
 class Me_gusta(Playlist):
+    '''Es broma, pero si quieres no lo es'''
     me_gustas = []
+
+    def __init__(self, ):
     pass
