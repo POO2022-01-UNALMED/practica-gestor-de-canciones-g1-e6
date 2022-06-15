0af5196a4512a956e9d4754f23fd0e24f57e287c
David Toro Arango <92808109+DavidToro1000@users.noreply.github.com>
2022-06-15 16:00:11 -0500

    Nuevas clases

--- Phyton/Gestor_canciones/src/gestorAplicacion/elementosLibreria/Favoritos.py
+++ Phyton/Gestor_canciones/src/gestorAplicacion/elementosLibreria/Favoritos.py
@@ -1,4 +1,10 @@
 from Playlist import Playlist
 class Favoritos(Playlist):
     favoritos = []
-    pass
+    def _init_(self, creador):
+      super()._init_(self, creador, "Favoritos")
+      Favoritos.favoritos.append(self)
+    def creadorPlaylist(self):
+        return   "Esta playlist fue creada por defecto"+"\n"+"Spotifoy todos los derechos reservados"
+    def toString():
+        return "La playlist Favoritos es creada por Spotifoy al crear el usuario"+"\n"+ "y tiene el id "+super().getId_real()
