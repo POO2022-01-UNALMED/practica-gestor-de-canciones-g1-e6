9a3f06410bc493c546d99ca232de81cb4e0b29f5
David Toro Arango <92808109+DavidToro1000@users.noreply.github.com>
2022-06-23 17:52:58 -0500

    avancitos

--- Phyton/Gestor_canciones/src/interfazGrafica/inicio.py
+++ Phyton/Gestor_canciones/src/interfazGrafica/inicio.py
@@ -13,7 +13,7 @@ class Inicio(Frame):
 
         self.ventana = tk.Tk()
         self.ventana.title("Inicio")
-        self.ventana.geometry("450x300")
+        self.ventana.geometry("680x420")
 
 
 
